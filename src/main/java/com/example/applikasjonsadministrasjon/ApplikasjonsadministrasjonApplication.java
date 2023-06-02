3c6bbfa425c58bdd9417ce962bb42022e00aa6f1
YRober <yomirobera@gmail.com>
2023-06-02 09:06:12 +0200

    applikasjons administrasjon repo

---
+++ src/main/java/com/example/applikasjonsadministrasjon/ApplikasjonsadministrasjonApplication.java
@@ -0,0 +1,13 @@
+package com.example.applikasjonsadministrasjon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplikasjonsadministrasjonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplikasjonsadministrasjonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/applikasjonsadministrasjon/ApplikasjonsadministrasjonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applikasjonsadministrasjon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplikasjonsadministrasjonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
