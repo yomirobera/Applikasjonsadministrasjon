fc65fb29c7dcf1ed932dfb9eee8ab7ecf5eda0d8
Yomiyu Robera Feyisa <91597707+yomirobera@users.noreply.github.com>
2023-06-02 09:39:55 +0200

    Applikasjonsadministrasjon repo

---
+++ Applikasjonsadministrasjon/src/main/java/com/example/applikasjonsadministrasjon/services/CrudService.java
@@ -0,0 +1,4 @@
+package com.example.applikasjonsadministrasjon.services;
+
+public interface CrudService {
+}
---
+++ Applikasjonsadministrasjon/src/main/java/com/example/applikasjonsadministrasjon/services/user/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.applikasjonsadministrasjon.services.user;
+
+public class UserServiceImpl {
+}
