80b3afa69fcaa563e40f6cbe3bd40db4643c47d3
Ddayisme <122437344+Ddayisme@users.noreply.github.com>
2023-08-17 14:38:54 +0200

    postman to chatgpt works

--- src/main/java/com/example/applikasjonsadministrasjon/ApplikasjonsadministrasjonApplication.java
+++ src/main/java/com/example/applikasjonsadministrasjon/ApplikasjonsadministrasjonApplication.java
@@ -2,11 +2,11 @@ package com.example.applikasjonsadministrasjon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class ApplikasjonsadministrasjonApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/applikasjonsadministrasjon/controllers/ChatBotController.java
@@ -0,0 +1,76 @@
+package com.example.applikasjonsadministrasjon.controllers;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.http.*;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@RestController
+@RequestMapping(path = "api/v1/")
+public class ChatBotController {
+    
+
+    @PostMapping("/ask")
+    public ResponseEntity<String> askChatBot(@RequestBody String userMessage){
+        String chatBotResponse= sendToOpenAI(userMessage);
+        return ResponseEntity.ok(chatBotResponse);
+    }
+
+    @Autowired
+private RestTemplate restTemplate;
+
+
+@Value("${OPENAI_API_KEY}")
+    private String openaiApiKey;
+
+private String sendToOpenAI(String userMessageInput) {
+    String openAiUrl = "https://api.openai.com/v1/chat/completions";  // URL may change, refer to OpenAI documentation
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.set("Authorization", "Bearer " + openaiApiKey);
+
+    
+     JSONObject systemMessage = new JSONObject();
+        systemMessage.put("role", "system");
+        systemMessage.put("content", "You are a helpful assistant.");
+
+        JSONObject userMessage = new JSONObject();
+        userMessage.put("role", "user");
+        userMessage.put("content", userMessageInput); // message from the user
+
+        JSONArray messagesArray = new JSONArray();
+        messagesArray.put(systemMessage);
+        messagesArray.put(userMessage);
+
+        JSONObject body = new JSONObject();
+        body.put("model", "gpt-3.5-turbo");
+        body.put("messages", messagesArray);
+
+    HttpEntity<String> entity = new HttpEntity<>(body.toString(), headers);
+
+    ResponseEntity<String> response = restTemplate.postForEntity(openAiUrl, entity, String.class);
+
+    // Extract and return the chatbot's response from the API's response.
+    JSONObject responseBody = new JSONObject(response.getBody());
+    String content = responseBody.getJSONArray("choices")
+    .getJSONObject(0)
+    .getJSONObject("message")
+    .getString("content")
+    .trim();
+    JSONObject contentObject = new JSONObject(content);
+    String assistantMessage = contentObject.getString("assistantMessage");
+
+    return assistantMessage;
+
+}
+}
